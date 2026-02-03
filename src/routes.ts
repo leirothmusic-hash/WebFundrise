7a8e1bcba32c628d9a2db6b28530bb1c00c85d05
leirothmusic-hash <leirothmusic@gmail.com>
2026-02-03 15:16:46 +0000

    routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -2,13 +2,19 @@ import { createBrowserRouter } from "react-router";
 import { Home } from "./components/Home";
 import { Info } from "./components/Info";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      Component: Home,
+    },
+    {
+      path: "/info",
+      Component: Info,
+    },
+  ],
   {
-    path: "/",
-    Component: Home,
-  },
-  {
-    path: "/info",
-    Component: Info,
-  },
-]);
+    // This tells the router your site is hosted at https://<user>.github.io/WebFundrise/
+    basename: "/WebFundrise",
+  }
+);
